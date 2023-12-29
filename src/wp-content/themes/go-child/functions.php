6487b8d81ac77bcb75e4d76550494c5911dd4bbd
cmoticeac <147082946+cmoticeac@users.noreply.github.com>
2023-12-29 20:44:32 +0100

    Style

--- src/wp-content/themes/go-child/functions.php
+++ src/wp-content/themes/go-child/functions.php
@@ -5,5 +5,11 @@
  * @package Go
  */
 
- // Agregar soporte para imágenes de encabezado
-
+ // Enqueue styles
+function enqueue_child_styles() {
+    // Importa los estilos del tema principal (padre)
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    // Agrega los estilos del tema hijo
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
+}
+add_action('wp_enqueue_scripts', 'enqueue_child_styles');
